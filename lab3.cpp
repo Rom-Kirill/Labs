2774ba6b1ab8658590b5ce103c20e2d691dd7612
Rom-Kirill <79634320+Rom-Kirill@users.noreply.github.com>
2021-04-12 17:38:06 +0600

    Add files via upload

---
+++ lab3.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include<ctime>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    srand(time(NULL));
+
+    const int SIZE = 10;
+    int array[SIZE];
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        array[i] = rand();
+    }
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        cout << array[i] << endl;
+    }
+
+
+    int maxvalue = array[0];
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (array[i] > maxvalue)
+        {
+            maxvalue = array[i];
+        }
+    }
+    cout << "max:" << maxvalue << endl;
+}
